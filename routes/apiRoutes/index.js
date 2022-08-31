dba0e9ec86cf821f37674107e68472512c3ada83
brian nelson <bn3l10@gmail.com>
2022-08-30 23:43:55 -0400

    added first prompt

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,8 @@
+// const express = require('express');
+// const router = express.Router();
+
+// router.use(require('./viewRoutes'));
+// router.use(require('./addRoutes'));
+// router.use(require('./updateRoutes'));
+
+// module.exports = router;
