a31af8ab687b01a224f2d6f6763fb6967f206cb2
brian nelson <bn3l10@gmail.com>
2022-08-30 22:34:32 -0400

    added tables and db

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR (30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL (10,2) NOT NULL,
+    department_id INT,
+
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    is_manager BOOLEAN NOT NULL,
+);
